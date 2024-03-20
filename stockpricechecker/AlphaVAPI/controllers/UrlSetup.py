1eb42d4f5474f31baa353d7aa69eeeadd8f358ca
IzziCooki <63527400+IzziCooki@users.noreply.github.com>
2024-03-19 20:57:32 -0400

    Delete stockpricechecker directory

--- stockpricechecker/AlphaVAPI/controllers/UrlSetup.py
+++
@@ -1,9 +0,0 @@
-import os
-
-    
-def CreateUrl(stock_symbol):
-    __apiKey = os.getenv("APIKEY")
-
-    url = f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={stock_symbol}&apikey={__apiKey}'
-
-    return url
